22698319cda67214808aadee8c35576dee0ff740
ChangChao-Tang <mrgmp2004@hotmail.com>
2018-01-16 01:36:31 +0800

    Initial Commit

---
+++ SafariWebviewDebuger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SafariWebviewDebuger
+//
+//  Created by ChangChao-Tang on 2018/1/16.
+//  Copyright © 2018年 ChangChao-Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
